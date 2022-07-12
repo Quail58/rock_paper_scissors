0de0342a4ef335fa25c85ded7aa62d9601a085eb
John Gibson <jhngbsn09@gmail.com>
2022-07-12 15:57:09 -0400

    Created a function that plays a round of the game

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function computerPlay(){
     // creates an Array of choices
     let choices = ["rock", "paper", "scissors"]
     // randomly picks from the Array
-    var choice = choices[Math.floor(Math.random() * choices.length)];
-    return choice;
+    var computer_choice = choices[Math.floor(Math.random() * choices.length)];
+    return computer_choice;
 }
 
 // a function that asks for a players input
@@ -15,3 +15,22 @@ function playerHand(){
     return player_choice.toLowerCase();        
 }
 
+function round(playerSelection, computerSelection){
+    let play = true;
+    while (play) {
+        if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "paper"){
+            alert("It's a tie!");
+            play = false;
+        } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+            alert("Player wins!");
+            play = false;
+        } else {
+            alert("Computer wins!");
+            play = false;
+        }
+    }
+}
+
+const playerSelection = playerHand();
+const computerSelection = computerPlay();
+console.log(round(playerSelection, computerSelection))
