cae7fb95e7d1b0523d240d26aedf68638ac70fff
John Gibson <jhngbsn09@gmail.com>
2022-07-14 13:52:18 -0400

    Fixed bugs and re worked code

--- script.js
+++ script.js
@@ -17,34 +17,45 @@ function playerHand(){
 
 // a function that plays one round of rock paper scissors
 function round(playerSelection, computerSelection){
-        if (playerSelection === "rock" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "paper"){
-            alert("It's a tie!")
-        } else if (playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+        if( (playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "paper") ){
+            alert("It's a tie!");
+        } else if ( (playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") ){
             alert("Player wins this round!");
             playerScore++;
         } else {
             alert("Computer wins this round!");
             computerScore++;
-        }
+        }  
 }
 
-// // trying to run a game that plays five times and picks a winner
-// function gameOn(){
-//     for (let i = 0; i < 5; i++){
-//         if (playerScore === 5){
-//             alert("Player Wins!");
-//         } else if (computerScore === 5){
-//             alert("Computer Wins!")
-//         } else {
-//             console.log(round(playerSelection, computerSelection))
-//         }
-//     }
-// }
+// trying to run a game that plays five times and picks a winner
+function gameOn(){
+    for (let i = 0; i < 5; i++){
+        playerSelection = playerHand();
+        computerSelection = computerPlay();
+        round(playerSelection, computerSelection);
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log("Player Score: " + playerScore);
+        console.log("Computer Score: " + computerScore);
+    
+        if(playerScore === 3){
+            alert("Player wins the game!");
+            break;
+        } else if(computerScore === 3){
+            alert("Computer wins the game!");
+            break;        
+        } else {
+            alert("Player score: " + playerScore + " Computer Score:" + computerScore);
+            alert("Pick again");
+        }
+    }
+    
+}
 
 // plays the game
 let playerScore = 0;
 let computerScore = 0;
-let score = "Player Score: " + playerScore + "Computer Score: " + computerScore
-const playerSelection = playerHand();
-const computerSelection = computerPlay();
-console.log(round(playerSelection, computerSelection));
+// playerSelection = "paper";
+// computerSelection = "paper";
+console.log(gameOn());
